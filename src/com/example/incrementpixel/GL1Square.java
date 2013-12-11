1bb7ba30219b39a2171cd3d6a92c8c01323f3390
kavlidlam <dianatlam@gmail.com>
2013-12-10 16:43:55 -0800

    Finished Increment Pixel
    App that allowed users to input an increment and the square will expand
    at that increment at each frame.

---
+++ src/com/example/incrementpixel/GL1Square.java
@@ -0,0 +1,89 @@
+package com.example.incrementpixel;
+
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class GL1Square {
+
+	public float vertices[] = {
+			-1.0f, 1.0f,
+			1.0f, 1.0f,
+			1.0f, -1.0f,
+			-1.0f, -1.0f,
+	};
+	
+	private float rgba[] = {
+			0, 0, 0, 1,
+			0, 0, 0, 1,
+			0, 0, 0, 1,
+			0, 0, 0, 1,
+	};
+
+       float[] newVertices = new float[8];
+	
+	private short[] pIndex = { 0, 1, 2, 0, 2, 3};
+	
+	private ShortBuffer pBuff;
+	
+	public FloatBuffer vertBuff, colorBuff;
+
+	public float size;
+	
+	public GL1Square(){
+		ByteBuffer bBuff = ByteBuffer.allocateDirect(vertices.length * 4);
+		bBuff.order(ByteOrder.nativeOrder());
+		vertBuff = bBuff.asFloatBuffer();
+		vertBuff.put(vertices);
+		vertBuff.position(0);
+		
+		ByteBuffer pbBuff = ByteBuffer.allocateDirect(pIndex.length * 2);
+		pbBuff.order(ByteOrder.nativeOrder());
+		pBuff = pbBuff.asShortBuffer();
+		pBuff.put(pIndex);
+		pBuff.position(0);
+		
+		ByteBuffer cBuff = ByteBuffer.allocateDirect(rgba.length * 4);
+		cBuff.order(ByteOrder.nativeOrder());
+		colorBuff = cBuff.asFloatBuffer();
+		colorBuff.put(rgba);
+		colorBuff.position(0);
+		
+	    size = 1;
+	}
+
+	public void scale(){
+	
+		vertBuff.clear();
+		for (int i = 0; i < vertices.length; i++)
+        	{
+        		newVertices[i] = vertices[i]*size;
+        		
+      		}
+
+      		vertBuff.put(newVertices);
+		vertBuff.position(0);
+	}
+	
+	public void draw(GL10 gl){
+		gl.glFrontFace(GL10.GL_CW);
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+		gl.glEnable(GL10.GL_CULL_FACE);
+		gl.glCullFace(GL10.GL_FRONT);
+		gl.glVertexPointer(2, GL10.GL_FLOAT, 0, vertBuff);
+		gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuff);
+		gl.glDrawElements(GL10.GL_TRIANGLES, pIndex.length, GL10.GL_UNSIGNED_SHORT, pBuff);
+		gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
+		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glDisable(GL10.GL_CULL_FACE);
+	}
+
+	
+}
+
+
